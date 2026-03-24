91270d9b45bd8229dc71b7af104e3468bbbac0d8
srcacahuate619 <srcacahuate619@gmail.com>
2026-03-24 15:22:27 -0600

    Delete generador_email.py

--- generador_email.py
+++
@@ -1,8 +0,0 @@
-nombre_usuario = input("Como te llamas?")
-nombre_usuario_normalizado = nombre_usuario.strip().lower().replace(" ", ".")
-nombre_empresa = input("Donde trabajas/estudias?")
-extension_dominio = ".com.mx"
-dominio_email_normalizado = nombre_empresa.strip().lower() + extension_dominio
-Email = f"{nombre_usuario_normalizado}@{dominio_email_normalizado}"
-print(Email)
-
